dd05c980623d300830354069477a7a3b4bb862d9
phenibut645 <doe481157@gmail.com>
2025-10-17 10:34:28 +0300

    As an app developer I want to get a list of all games to show in my app [Delivers #430024]

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 
 app.get("/heroes", (req, res) => {
-    res.send(["Shadow Fiend", "Spectre", "Earthshaker", "Tinker", "Broodmother", "Lone Druid", "Wraith King", "Anti Mage", "Phantom Assasin", "Phantom Lancer", "Sand King", "Queen Of Pain", "Ogre Magi", "Axe", "Chaos Knight", "Terrorblade", "Oracle", "Puck", ""])
+    res.send(["Shadow Fiend", "Spectre", "Earthshaker", "Tinker", "Broodmother", "Lone Druid", "Wraith King", "Anti Mage", "Phantom Assasin", "Phantom Lancer", "Sand King", "Queen Of Pain", "Ogre Magi", "Axe", "Chaos Knight", "Terrorblade", "Oracle", "Puck"])
 })
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
