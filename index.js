718791280a1be5e3a39632c41db0316b7f762056
phenibut645 <doe481157@gmail.com>
2025-10-17 10:17:13 +0300

    Invalid var name has fixed

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const port = 8080;
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swahherDocument))
+app.get("/heroes", (req, res) => {
+    res.send(["Shadow Fiend", "Spectre", "Earthshaker", "Tinker", "Broodmother", "Lone Druid", "Wraith King", "Anti Mage", "Phantom Assasin", "Phantom Lancer", "Sand King", "Queen Of Pain", "Ogre Magi", "Axe", "Chaos Knight", "Terrorblade", "Oracle", "Puck", ""])
+})
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
